bbf47c62b5aa42e1d065f131c33cca7eb18e678a
rhickwelfloresvaldez <rhickwel.valdez@lorma.edu>
2025-02-18 21:06:34 +0800

    added functionality and revised widget states

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,80 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    List<WeatherData> locations = [
-      WeatherData(1, 'Current Location', 'Dhaka', 'Thunder', 20, true),
-      WeatherData(2, 'United States', 'California', 'Clear', 6, false),
-      WeatherData(3, 'China', 'Beijing', 'Mostly Sunny', 5, false),
-      WeatherData(4, 'Russia', 'Moscow', 'Cloudy', -04, false)
-    ];
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  List<WeatherData> locations = [
+    WeatherData(1, 'Current Location', 'Dhaka', 'Thunder', 20, true),
+    WeatherData(2, 'United States', 'California', 'Clear', 6, false),
+    WeatherData(3, 'China', 'Beijing', 'Mostly Sunny', 5, false),
+    WeatherData(4, 'Russia', 'Moscow', 'Cloudy', -4, false)
+  ];
+
+  late WeatherData selectedWeather;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedWeather = locations.firstWhere((location) => location.isSelected);
+  }
+
+  void updateSelectedWeather(WeatherData newWeather) {
+    setState(() {
+      for (var location in locations) {
+        location.isSelected = false;
+      }
+      newWeather.isSelected = true;
+      selectedWeather = newWeather;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: 
-          Text(
+        appBar: AppBar(
+          title: Text(
             'Weather',
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-            textAlign: TextAlign.start,
           ),
         ),
         body: Container(
-          padding: EdgeInsets.only(top: 35, right: 15, bottom: 35, left: 15),
+          padding: EdgeInsets.all(15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              WeatherCard(weatherData: locations.first),
+              WeatherCard(weatherData: selectedWeather),
               SizedBox(height: 18),
               Text(
                 'Around the world',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500
-                ),
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
               ),
               SizedBox(height: 14),
-              for (var location in locations)
-                if (!location.isSelected)
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 14),
-                    child: WeatherCard(weatherData: location),
-                  ),
+              Column(
+                children: locations
+                    .where((location) => !location.isSelected)
+                    .map((location) => Padding(
+                          padding: const EdgeInsets.only(bottom: 14),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            onPressed: () => updateSelectedWeather(location),
+                            child: WeatherCard(weatherData: location),
+                          ),
+                        ))
+                    .toList(),
+              ),
             ],
           ),
         ),
@@ -53,44 +84,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// ignore: must_be_immutable
-class WeatherCard extends StatefulWidget {
-  WeatherCard({super.key, this.weatherData});
+class WeatherCard extends StatelessWidget {
+  final WeatherData weatherData;
 
-  WeatherData? weatherData;
+  const WeatherCard({super.key, required this.weatherData});
 
-  @override
-  State<WeatherCard> createState() => _WeatherCardState();
-}
-
-class _WeatherCardState extends State<WeatherCard> {
   String getImage(String weather) {
-    late String imgPath;
-
     switch (weather) {
       case 'Thunder':
-        imgPath = 'assets/img/heavy_rain.png';
-        break;
+        return 'assets/img/heavy_rain.png';
       case 'Clear':
-        imgPath = 'assets/img/snow.png';
-        break;
+        return 'assets/img/snow.png';
       case 'Mostly Sunny':
-        imgPath = 'assets/img/sunny.png';
-        break;
+        return 'assets/img/sunny.png';
       case 'Cloudy':
-        imgPath = 'assets/img/thunder.png';
-        break;
+        return 'assets/img/thunder.png';
+      default:
+        return 'assets/img/default.png';
     }
-
-    return imgPath;
-  }
-
-  Text displayText(String text, double fs, FontWeight fw, TextAlign alignText) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: fs, fontWeight: fw, color: Color(0xFFFFFFFF)),
-      textAlign: alignText,
-    );
   }
 
   @override
@@ -109,30 +120,30 @@ class _WeatherCardState extends State<WeatherCard> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    displayText(widget.weatherData!.country, 14,
-                        FontWeight.normal, TextAlign.start),
-                    displayText(widget.weatherData!.city, 22, FontWeight.w500,
-                        TextAlign.start),
+                    Text(weatherData.country,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white)),
+                    Text(weatherData.city,
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white)),
                   ],
                 ),
-                Image.asset(getImage(widget.weatherData!.weather))
+                Image.asset(getImage(weatherData.weather)),
               ],
             ),
             SizedBox(height: 14),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(
-                  widget.weatherData!.weather,
-                  style: TextStyle(color: Color(0xFFFFFFFF)),
-                ),
-                Text(
-                  '${widget.weatherData!.temperature}C',
-                  style: TextStyle(color: Color(0xFFFFFFFF)),
-                )
+                Text(weatherData.weather, style: TextStyle(color: Colors.white)),
+                Text('${weatherData.temperature}C',
+                    style: TextStyle(color: Colors.white)),
               ],
-            )
+            ),
           ],
         ),
       ),
@@ -148,8 +159,14 @@ class WeatherData {
   final int _temperature;
   bool _isSelected;
 
-  WeatherData(this._locationId, this._country, this._city, this._weather,
-      this._temperature, this._isSelected);
+  WeatherData(
+    this._locationId, 
+    this._country, 
+    this._city, 
+    this._weather,
+    this._temperature, 
+    this._isSelected
+  );
 
   int get locationId => _locationId;
   String get country => _country;
